158fda86ca484a998ccb3aff969ab049fbb35441
Lucas Duarte <145047184+LucasDuarteTec@users.noreply.github.com>
2023-11-26 14:24:02 -0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function calcularIMC() {
-    let peso = document.getElementById("peso").value;
-    let altura = document.getElementById("altura").value;
-    let imc = peso/(altura**2);
-    document.getElementById("result").innerHTML = "Seu IMC é: " + parseInt(imc);
-}
